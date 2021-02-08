3b1024f7b83912e551010286100ee44ca1fe920f
Mairi <mairi.mcqueer@gmail.com>
2021-02-08 18:00:19 +0000

    added darkmode (hopefully)

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,6 +94,35 @@ var BeautifulJekyllJS = {
   }
 };
 
+document.addEventListener('turbolinks:load', themeChange);
+
+function themeChange(){
+    // Select our toggle button
+    let button = document.querySelector('.theme-toggle');
+
+    // Add an event listener for a click
+    button.addEventListener('click', function(e){
+        // Check the current data-theme value
+        let currentTheme = document.documentElement.getAttribute('data-theme');
+        if(currentTheme === 'pastelPink') {
+            transition();
+            document.documentElement.setAttribute('data-theme', 'cyberPunk');
+        } else {
+            transition();
+            document.documentElement.setAttribute('data-theme','pastelPink');
+        }
+    });
+
+    // Adds the 'transition' class to <html> for CSS fun
+    let transition = () =>{
+        document.documentElement.classList.add('transition');
+        window.setTimeout(()=>{
+            document.documentElement.classList.remove('transition');
+        }, 1000);
+    }
+}
+
+
 // 2fc73a3a967e97599c9763d05e564189
 
 document.addEventListener('DOMContentLoaded', BeautifulJekyllJS.init);
